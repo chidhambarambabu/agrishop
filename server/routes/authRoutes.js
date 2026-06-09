5be2242c12611d6f6752bba5d652f2dd42a07b41
chidhambaram <chidhambaram.2023@vitstudent.ac.in>
2026-06-09 14:24:21 +0530

    Force update auth files

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const { register, verifyOTP, login, resendOTP } = require('../controllers/authController');
+const authController = require('../controllers/authController');
 
-router.post('/register', register);
-router.post('/verify-otp', verifyOTP);
-router.post('/login', login);
-router.post('/resend-otp', resendOTP);
+router.post('/register', authController.register);
+router.post('/login', authController.login);
 
 module.exports = router;
